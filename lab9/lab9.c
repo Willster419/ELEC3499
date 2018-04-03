f2c1a9fd6f0289894bf29257b4ae1a965cd90d32
Willster419 <willster419@outlook.com>
2018-04-03 15:36:42 -0400

    might have finished lab9, need to test it

--- lab9/lab9.c
+++ lab9/lab9.c
@@ -20,7 +20,7 @@
 #define RANDOM_PAGE_TRACE_LENGTH 10
 #define MIN_PAGE_NUMBER 0
 #define MAX_PAGE_NUMBER 31
-#define MODE 0//0=LRU,1=OPT
+#define MODE 1//0=LRU,1=OPT
 
 //globals
 int num_faults = 0;
@@ -31,12 +31,13 @@ int page_trace_length = 0;
 
 //method sigs
 void LRU(int address);
-void optimal(int address);
+void optimal(int address, int current_page_trace_index);
 void add_page(int address);
 bool is_full();
 void inc_staleness();
-void evict_and_replace(int address);
+void evict_and_replace_LRU(int address);
 void evict_and_replace_old(int address);
+void evict_and_replace_opt(int address, int current_page_trace_index);
 void print_page_table();
 void add_page_old();
 bool find_page(int address);
@@ -48,6 +49,8 @@ struct Page
   bool is_tail;
   int staleness;
   bool is_empty;
+  bool used_in_future;
+  int future_staleness;
   struct Page *next;
   struct Page *prev;
 };
@@ -73,15 +76,40 @@ int main(int argc,char* argv[])
     int counter = 0;
     //parse the page trace to an ints
     char *page_trace_char;
+    char *secondString;
+    //allocate the required memory for the second copy
+    secondString = malloc(sizeof(char)*strlen(argv[2]));
+    strcpy(secondString,argv[2]);
+    //if(DEBUG)
+    //  printf("DEBUG: copied string\n");
+    //first get the total number of strings to parse
     page_trace_char = strtok(argv[2], delim);
+    while (page_trace_char != NULL)
+    {
+      page_trace_length++;
+      page_trace_char = strtok(NULL,delim);
+    }
+    if(DEBUG)
+      printf("DEBUG: number of page traces: %d\n",page_trace_length);
+    //second allocate memory and create the page table
+    page_trace = malloc(sizeof(int)*page_trace_length);
+    page_trace_char = strtok(secondString, delim);
+    //if(DEBUG)
+    //  printf("DEBUG: first char parsed: %s\n",page_trace_char);
     while (page_trace_char != NULL)
     {
       page_trace[counter++] = atoi(page_trace_char);
       if(DEBUG)
-        printf("DEBUG: int parsed: %d, index %d\n", page_trace[counter-1], counter);
+        printf("DEBUG: int attached: %d, index %d\n", page_trace[counter-1], counter);
       page_trace_char = strtok(NULL,delim);
+      //if(DEBUG)
+      //  printf("DEBUG: next char parsed: %s\n",page_trace_char);
     }
-    page_trace_length = counter;
+    /*
+    if(DEBUG)
+      printf("ENDING NOW\n");
+    return 0;
+    */
   }
   else
   {
@@ -143,12 +171,10 @@ int main(int argc,char* argv[])
     }
     last = temp;
   }
-  //print current page table?
-  if(DEBUG)
-  {
-    printf("DEBUG: print page table\n");
-    print_page_table();
-  }
+  //print current page table
+  printf("at start, page table is:\n");
+  print_page_table();
+  
   //run the page simulation
   if(DEBUG)
       printf("DEBUG: simulation starts, MODE=%d, (%s)\n",MODE, MODE==0? "LRU":"OPT");
@@ -160,7 +186,8 @@ int main(int argc,char* argv[])
         LRU(page_trace[i]);
       break;
       case 1:
-        optimal(page_trace[i]);
+        //i in this case can be used an an index for where we are in the page trace
+        optimal(page_trace[i],i);
       break;
       default:
         printf("ERROR: Invalid case from MODE: %d\n",MODE);
@@ -183,7 +210,7 @@ void LRU(int address)
     if(is_full())
     {
       printf("page table is full, evict/replace and inc num_faults\n");
-      evict_and_replace(address);
+      evict_and_replace_LRU(address);
     }
     else
     {
@@ -195,9 +222,26 @@ void LRU(int address)
   inc_staleness();
 }
 
-void optimal(int address)
+void optimal(int address, int current_page_trace_index)
 {
-  
+  bool page_found = find_page(address);
+  if(!page_found)
+  {
+    printf("page fault found, requested address=%d\n",address);
+    if(is_full())
+    {
+      printf("page table is full, evict/replace and inc num_faults\n");
+      evict_and_replace_opt(address,current_page_trace_index);
+    }
+    else
+    {
+      printf("page table not full, \"add\" to buttom of stack and inc num_faults\n");
+      add_page(address);
+    }
+    num_faults++;
+  }
+  //staleness does not matter i this one
+  //inc_staleness();
 }
 
 bool is_full()
@@ -247,8 +291,17 @@ void print_page_table()
   int counter = 0;
   while(ref->next != NULL)
   {
-    printf("Page entry %d: address=%d, staleness=%d, is_head=%d, is_tail=%d, is_empty=%d\n",
-    counter++,ref->address,ref->staleness,ref->is_head,ref->is_tail,ref->is_empty);
+    switch(MODE)
+    {
+      case 0://LRU
+        printf("Page entry %d: address=%d, is_head=%d, is_tail=%d, is_empty=%d, staleness=%d,\n",
+          counter++,ref->address,ref->is_head,ref->is_tail,ref->is_empty,ref->staleness);
+      break;
+      case 1://OPT
+        printf("Page entry %d: address=%d, is_head=%d, is_tail=%d, is_empty=%d\n",
+          counter++,ref->address,ref->is_head,ref->is_tail,ref->is_empty);
+      break;
+    }
     ref = ref->next;
   }
 }
@@ -284,7 +337,7 @@ void evict_and_replace_old(int address)
   ref->prev = NULL;
 }
 
-void evict_and_replace(int address)
+void evict_and_replace_LRU(int address)
 {
   //find the most stale page
   struct Page *ref;
@@ -309,6 +362,94 @@ void evict_and_replace(int address)
       most_stale->address,address);
   most_stale->address = address;
   most_stale->staleness = -1;
+}
+
+void evict_and_replace_opt(int address, int current_page_trace_index)
+{
+  //reset used_in_future and future_staleness
+  struct Page *ref;
+  ref = head;
+  while(ref->next != NULL)
+  {
+    ref->used_in_future = false;
+    ref->future_staleness = 0;
+    ref = ref->next;
+  }
+  if(DEBUG)
+    printf("DEBUG: reset used_in_future and future_staleness values\n");
+  //also know that getting here means that the page table is full
+  //start counting from the current place in trace (not including current address)
+  //count out (if not used in ifture) the future staleness
+  int cycle_counter = 0;
+  for(int i = current_page_trace_index+1; i < page_trace_length; i++)
+  {
+    if(DEBUG)
+      printf("DEBUG: checking for page trace index %d, value=%d\n",i,page_trace[i]);
+    struct Page *temp;
+    temp = head;
+    while(temp->next != NULL)
+    {
+      if(!temp->used_in_future)
+      {
+        temp->future_staleness++;
+      }
+      if(page_trace[i]==temp->address && !temp->used_in_future)
+      {
+        temp->used_in_future = true;
+        if(DEBUG)
+          printf("DEBUG: page at address %d used %d cycles in future, bool set\n",temp->address,cycle_counter+1);
+      }
+      temp = temp->next;
+    }
+    cycle_counter++;
+  }
+  //if after done, we find a not_used_in_future, evict
+  ref = head;
+  //re-use cycle counter
+  cycle_counter = 0;
+  bool found_page_for_removal = false;
+  struct Page *max_future_stale_page;
+  while(ref->next != NULL)
+  {
+    if(!ref->used_in_future)
+    {
+      if(DEBUG)
+        printf("DEBUG: found that page %d is never used in future, selected for removal (address=%d)\n",cycle_counter,ref->address);
+      max_future_stale_page = ref;
+      found_page_for_removal = true;
+      break;
+    }
+    cycle_counter++;
+    ref = ref->next;
+  }
+  //else, find the maximum future staleness and evict it
+  if(!found_page_for_removal)
+  {
+    //re-use cycle counter again
+    cycle_counter = 0;
+    int max_future_stale = -1;
+    ref=head;
+    while(ref->next != NULL)
+    {
+      if(ref->future_staleness > max_future_stale)
+      {
+        max_future_stale_page = ref;
+        max_future_stale=ref->future_staleness;
+      }
+      cycle_counter++;
+      ref = ref->next;
+    }
+    if(DEBUG)
+      printf("DEBUG: found page that is most future stale (value=%d), selected for removal (address=%d)\n",
+    max_future_stale_page->future_staleness,max_future_stale_page->address);
+  }
+  //we now have the most stale version
+  //evict the old address and add the new address
+  //still has data, don't touch refrences, etc
+  if(DEBUG)
+    printf("DEBUG: evicted page address=%d, new page address = %d\n",
+      max_future_stale_page->address,address);
+  max_future_stale_page->address = address;
 }
 
 void add_page(int address)
